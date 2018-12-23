12aa9a1718fa13b04a2f28abc2904cfefd618854
karabanb <b.karaban@me.com>
2018-12-23 20:56:20 +0100

    adding email_vars function

---
+++ R/email_vars.R
@@ -0,0 +1,51 @@
+
+#' Title Extract features form email address, name and surname
+#'
+#' @param x - a data.frame object with email address, name and surname.
+#' @param address - column's name with email address
+#' @param name - column's name with email's owner name
+#' @param surname - column's name with of email's owner surname
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+email_vars <- function(x, address, name, surname) {
+
+  vars <- c(address, name, surname)
+  x <- x[, vars]
+
+  # checking condtitions
+
+  if (!is.data.frame(x))
+    stop("object x must be a data.frame!")
+
+  if (!all(sapply(x, class) %in% c("factor", "character")))
+    stop("Variables must be factors or characters!")
+
+  if (!all(grepl("^[[:alnum:].-_]+@[[:alnum:].-]+$", x[,"address"])))
+    warning("Your data contains not validated emails!")
+
+  # converting factors to characters
+
+  x <- as.data.frame(lapply(x, as.character), stringsAsFactors = FALSE)
+
+  # converting name and surname to LATIN-ASCII and trim
+
+  x[, name] <- stringi::stri_trans_general(x[, name], "LATIN-ASCII")
+  x[, surname] <- stringi::stri_trans_general(x[,surname], "LATIN-ASCII")
+  x[, name] <- stringi::stri_trim_both(x[, name])
+  x[, surname] <- stringi::stri_trim_both(x[, surname])
+
+  # making features based on email, name and surname
+
+  output<- data.frame(address = x[ , address],
+                      email_name = grepl(x[, name], x[, address], ignore.case = TRUE),
+                      email_surname = grepl(x[, surname], x[, address], ignore.case = TRUE),
+                      email_both = grepl(x[, name], x[, address], ignore.case = TRUE) &
+                        grepl(x[, surname], x[, address], ignore.case = TRUE)
+                      )
+  return(output)
+
+}
